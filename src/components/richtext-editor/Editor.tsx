ae668ce69132353efca2418fecff6539cece9b3c
Prajwal Dhungana <dhunganaprajwal1@gmail.com>
2024-12-08 19:17:58 -0600

    Added feature to save the changes to local storage and load it so that the content on editor is not erased even when page is refreshed

--- src/components/richtext-editor/Editor.tsx
+++ src/components/richtext-editor/Editor.tsx
@@ -4,12 +4,10 @@ import { BlockNoteView } from '@blocknote/mantine';
 import '@blocknote/mantine/style.css';
 import { useCreateBlockNote } from '@blocknote/react';
 import { useTheme } from 'next-themes';
-import React from 'react';
+import { useCallback, useEffect } from 'react';
 
 export default function Editor() {
   const { theme, systemTheme } = useTheme();
-  const [markdown, setMarkdown] = React.useState<string>();
-
   const resolvedTheme = theme === 'system' ? systemTheme : theme;
   const blockNoteTheme =
     resolvedTheme === 'dark' || resolvedTheme === 'light'
@@ -18,10 +16,23 @@ export default function Editor() {
 
   const editor = useCreateBlockNote();
 
-  const onChange = async () => {
+  // Load initial Markdown from localStorage
+  useEffect(() => {
+    const loadMarkdownFromStorage = async () => {
+      const savedMarkdown = localStorage.getItem('savedMarkdown');
+      if (savedMarkdown) {
+        const blocks = await editor.tryParseMarkdownToBlocks(savedMarkdown);
+        editor.replaceBlocks(editor.document, blocks);
+      }
+    };
+    loadMarkdownFromStorage();
+  }, [editor]);
+
+  // Save Markdown to localStorage on editor change
+  const onChange = useCallback(async () => {
     const markdown = await editor.blocksToMarkdownLossy(editor.document);
-    setMarkdown(markdown);
-  };
+    localStorage.setItem('savedMarkdown', markdown);
+  }, [editor]);
 
   return (
     <BlockNoteView
