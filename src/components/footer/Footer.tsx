f3d42865c4c3ac245422e5c6c58ad4622699d0b1
Prajwal Dhungana <dhunganaprajwal1@gmail.com>
2024-12-10 22:00:50 -0600

    Updated footer height and added padding x to home component

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
-      className='w-[100dvw] h-[60px] px-1 md:px-14 justify-center items-center flex flex-row bg-background/80 backdrop-blur-md bg-opacity-50 dark:bg-opacity-50 border-t-[1px] border-slate-200 dark:border-stone-700 overflow-x-hidden'
+      className='w-[100dvw] h-[70px] px-1 md:px-14 justify-center items-center flex flex-row bg-background/80 backdrop-blur-md bg-opacity-50 dark:bg-opacity-50 border-t-[1px] border-slate-200 dark:border-stone-700 overflow-x-hidden'
     >
       <div className='flex flex-row gap-0.5 justify-center items-center font-mono'>
         <p className='text text-sm m text-light-text-primary dark:text-dark-text-primary'>
