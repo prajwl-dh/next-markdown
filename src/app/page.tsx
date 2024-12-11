e670a745224fe13b6e8394d15d65114b70d39bbf
Prajwal Dhungana <dhunganaprajwal1@gmail.com>
2024-12-10 21:36:30 -0600

    Changed The styling of home page and footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,16 +7,16 @@ export default function App() {
   return (
     <>
       <div className='h-dvh w-dvw fixed pattern-wavy pattern-blue-500 pattern-bg-white pattern-size-6 pattern-opacity-5 z-0' />
-      <div className='flex flex-col justify-between mt-[70px]'>
+      <div className='flex flex-col justify-between mt-[80px]'>
         <Navbar
           initial={{ opacity: 0, y: -10 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ delay: 0.1, type: 'spring' }}
         />
-        <div className='w-screen h-[calc(100dvh-70px-60px)] px-1 md:px-14 flex flex-col items-center'>
+        <div className='w-screen min-h-[calc(100dvh-80px)] px-1 md:px-14 flex flex-col justify-between items-center overflow-x-hidden'>
           <Home />
+          <Footer />
         </div>
-        <Footer />
       </div>
     </>
   );
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,10 +5,10 @@ import { motion } from 'framer-motion';
 export default function Footer() {
   return (
     <motion.div
-      initial={{ opacity: 0, y: 10 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ delay: 0.1, type: 'spring' }}
-      className='w-full h-[60px] px-1 md:px-14 justify-center items-center flex flex-row bg-background/80 backdrop-blur-md bg-opacity-50 dark:bg-opacity-50 border-t-[1px] border-slate-200 dark:border-stone-700 fixed bottom-0 z-20'
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className='w-[100dvw] h-[60px] px-1 md:px-14 justify-center items-center flex flex-row bg-background/80 backdrop-blur-md bg-opacity-50 dark:bg-opacity-50 border-t-[1px] border-slate-200 dark:border-stone-700 overflow-x-hidden'
     >
       <div className='flex flex-row gap-0.5 justify-center items-center font-mono'>
         <p className='text text-sm m text-light-text-primary dark:text-dark-text-primary'>
