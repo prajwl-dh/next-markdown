95433a70565a69a8bd1fc3ff40eb1814530503c5
Prajwal Dhungana <dhunganaprajwal1@gmail.com>
2024-12-18 13:51:00 -0600

    Added export feature for markdown editor

--- src/components/content-export/CodeBlock.tsx
+++ src/components/content-export/CodeBlock.tsx
@@ -1,11 +1,17 @@
 'use client';
 
 import hljs from 'highlight.js';
-import 'highlight.js/styles/atom-one-dark.css'; // Choose your theme
+import 'highlight.js/styles/night-owl.css';
 import { ReactNode, useEffect, useRef, useState } from 'react';
 import { LuCheck, LuCopy } from 'react-icons/lu';
 
-export default function CodeBlock({ children }: { children: ReactNode }) {
+export default function CodeBlock({
+  children,
+  variant,
+}: {
+  children: ReactNode;
+  variant: string;
+}) {
   const codeRef = useRef<HTMLPreElement>(null);
   const [copySuccess, setCopySuccess] = useState(false);
 
@@ -29,18 +35,23 @@ export default function CodeBlock({ children }: { children: ReactNode }) {
   };
 
   return (
-    <div className='flex flex-col rounded-md w-full bg-[#282c34]'>
-      <button
-        onClick={copyToClipboard}
-        className='bg-teal-600 text-white rounded px-2 py-1 text-sm self-end z-50'
-      >
-        {copySuccess ? <LuCheck /> : <LuCopy />}
-      </button>
+    <div className='flex flex-col rounded-md bg-[#011627] overflow-hidden'>
+      <div className='flex flex-row justify-between border-b-[1px] border-gray-500 p-2'>
+        <p className='text-white font-semibold'>{variant}</p>
+        <button
+          onClick={copyToClipboard}
+          className='bg-teal-600 text-white rounded px-2 py-1 text-sm self-end z-50'
+        >
+          {copySuccess ? <LuCheck /> : <LuCopy />}
+        </button>
+      </div>
       <pre
         ref={codeRef}
-        className='rounded-md p-4 w-full max-w-lg max-h-[50dvh] overflow-auto bg-white text-black'
+        className={`rounded-md py-2 pl-2 w-auto overflow-x-scroll overflow-y-scroll antialiased z-50 ${
+          variant !== 'JSON' ? 'h-[30dvh]' : 'h-[5dvh]'
+        }`}
       >
-        <code>{children}</code>
+        <code className='antialiased'>{children}</code>
       </pre>
     </div>
   );
